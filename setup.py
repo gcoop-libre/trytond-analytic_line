63d9f21f2474d616871f2b3cd5a0b023aac390e7
Adrián Bernardi <adrian.bernardi@gmail.com>
2021-11-09 12:49:36 -0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,4 +1,5 @@
 #!/usr/bin/env python3
+# This file is part of the analytic_line module for Tryton.
 # The COPYRIGHT file at the top level of this repository contains
 # the full copyright notices and license terms.
 
@@ -6,19 +7,24 @@ import io
 import os
 import re
 from configparser import ConfigParser
-from setuptools import setup, find_packages
+from setuptools import setup
+
+MODULE = 'analytic_line'
+PREFIX = 'trytonar'
+MODULE2PREFIX = {}
 
 
-def read(fname, slice=None):
-    content = io.open(
+def read(fname):
+    return io.open(
         os.path.join(os.path.dirname(__file__), fname),
         'r', encoding='utf-8').read()
-    if slice:
-        content = '\n'.join(content.splitlines()[slice])
-    return content
 
 
 def get_require_version(name):
+    #if name.startswith('trytonar_'):
+        #return ''
+    if name in LINKS:
+        return '%s@%s' % (name, LINKS[name])
     if minor_version % 2:
         require = '%s >= %s.%s.dev0, < %s.%s'
     else:
@@ -38,32 +44,30 @@ version = info.get('version', '0.0.1')
 major_version, minor_version, _ = version.split('.', 2)
 major_version = int(major_version)
 minor_version = int(minor_version)
-name = 'trytonar_analytic_line'
-
-download_url = 'https://github.com/gcoop-libre/trytond-analytic_line/tree/%s.%s' % (
-    major_version, minor_version)
+series = '%s.%s' % (major_version, minor_version)
 if minor_version % 2:
-    version = '%s.%s.dev0' % (major_version, minor_version)
-    download_url = (
-        'git+http://github.com/gcoop-libre/%s#egg=%s-%s' % (
-            name.replace('trytonar','trytond'), name, version))
-local_version = []
-for build in ['CI_BUILD_NUMBER', 'CI_JOB_NUMBER', 'CI_JOB_ID']:
-    if os.environ.get(build):
-        local_version.append(os.environ[build])
-if local_version:
-    version += '+' + '.'.join(local_version)
+    branch = 'master'
+else:
+    branch = series
 
-requires = ['requests']
+download_url = 'https://github.com/gcoop-libre/trytond-analytic_line/tree/%s' % branch
+
+LINKS = {}
+
+requires = []
 for dep in info.get('depends', []):
     if not re.match(r'(ir|res)(\W|$)', dep):
-        requires.append(get_require_version('trytond_%s' % dep))
+        module_name = '%s_%s' % (MODULE2PREFIX.get(dep, 'trytond'), dep)
+        requires.append(get_require_version(module_name))
+
 requires.append(get_require_version('trytond'))
 
 tests_require = [get_require_version('proteus')]
-dependency_links = []
+dependency_links = list(LINKS.values())
+if minor_version % 2:
+    dependency_links.append('https://trydevpi.tryton.org/')
 
-setup(name=name,
+setup(name='%s_%s' % (PREFIX, MODULE),
     version=version,
     description='',
     long_description=read('README.rst'),
@@ -78,16 +82,15 @@ setup(name=name,
         "Source Code": 'https://github.com/gcoop-libre/trytond-analytic_line',
         },
     keywords='tryton, sale, analytic',
-    package_dir={'trytond.modules.analytic_line': '.'},
-    packages=(
-        ['trytond.modules.analytic_line']
-        + ['trytond.modules.analytic_line.%s' % p
-            for p in find_packages()]
-        ),
+    package_dir={'trytond.modules.%s' % MODULE: '.'},
+    packages=[
+        'trytond.modules.%s' % MODULE,
+        'trytond.modules.%s.tests' % MODULE,
+        ],
     package_data={
-        'trytond.modules.analytic_line': (info.get('xml', [])
-            + ['tryton.cfg', 'view/*.xml', 'locale/*.po', '*.fodt',
-                'icons/*.svg', 'tests/*.rst']),
+        'trytond.modules.%s' % MODULE: (info.get('xml', []) + [
+            'tryton.cfg', 'view/*.xml', 'locale/*.po', '*.fodt',
+            '*.fods', 'icons/*.svg', 'tests/*.rst']),
         },
     classifiers=[
         'Development Status :: 5 - Production/Stable',
@@ -98,44 +101,28 @@ setup(name=name,
         'Intended Audience :: Legal Industry',
         'License :: OSI Approved :: '
         'GNU General Public License v3 or later (GPLv3+)',
-        'Natural Language :: Bulgarian',
-        'Natural Language :: Catalan',
-        'Natural Language :: Chinese (Simplified)',
-        'Natural Language :: Czech',
-        'Natural Language :: Dutch',
         'Natural Language :: English',
-        'Natural Language :: Finnish',
-        'Natural Language :: French',
-        'Natural Language :: German',
-        'Natural Language :: Hungarian',
-        'Natural Language :: Indonesian',
-        'Natural Language :: Italian',
-        'Natural Language :: Persian',
-        'Natural Language :: Polish',
-        'Natural Language :: Portuguese (Brazilian)',
-        'Natural Language :: Russian',
-        'Natural Language :: Slovenian',
         'Natural Language :: Spanish',
-        'Natural Language :: Turkish',
         'Operating System :: OS Independent',
         'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.5',
         'Programming Language :: Python :: 3.6',
         'Programming Language :: Python :: 3.7',
         'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
         'Programming Language :: Python :: Implementation :: CPython',
         'Programming Language :: Python :: Implementation :: PyPy',
         'Topic :: Office/Business',
+        'Topic :: Office/Business :: Financial :: Accounting',
         ],
     license='GPL-3',
-    python_requires='>=3.5',
+    python_requires='>=3.6',
     install_requires=requires,
     dependency_links=dependency_links,
     zip_safe=False,
     entry_points="""
     [trytond.modules]
-    analytic_line = trytond.modules.analytic_line
-    """,  # noqa: E501
+    %s = trytond.modules.%s
+    """ % (MODULE, MODULE),
     test_suite='tests',
     test_loader='trytond.test_loader:Loader',
     tests_require=tests_require,
