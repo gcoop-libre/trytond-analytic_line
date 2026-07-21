2b64df84c83b3d82d94d9018a400e6a559eabe60
Luciano Rossi <lukio@gcoop.coop>
2026-07-21 15:22:53 -0300

    migration: Tryton 8.0 - hatchling build, register block
    - version bump to 8.0.0
    - [register] block in tryton.cfg with model entries (Move, MoveLine, Line)
    - __init__.py cleaned (Pool.register removed)
    - pyproject.toml created (hatchling + hatch-tryton-ar)
    - tox.ini updated (py310+ envs, coverage + xmlrunner)
    - setup.py removed

--- __init__.py
+++ __init__.py
@@ -1,13 +1,2 @@
 # The COPYRIGHT file at the top level of this repository contains
 # the full copyright notices and license terms.
-
-from trytond.pool import Pool
-from . import line
-
-
-def register():
-    Pool.register(
-        line.Move,
-        line.MoveLine,
-        line.Line,
-        module='analytic_line', type_='model')
